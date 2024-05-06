890132553f4c7d3c3ba9075e9e410d237979e7ad
erstinn <erstin.adean@gmail.com>
2024-05-06 14:28:50 +0800

    update db fs

--- src/utils/portfolio-data-utils.ts
+++ src/utils/portfolio-data-utils.ts
@@ -10,17 +10,25 @@ const apiURL = 'http://localhost:8055'
 const email = 'admin@example.com'
 const password = 'd1r3ctu5'
 
+const setupDirectusConnection = async function() {
+    const client = createDirectus(apiURL).with(authentication()).with(rest());
+    await client.login(email, password);
+    return client;
+}
+
+const client = await setupDirectusConnection(); //dont know if works outside; prob this is on some main file
+
+
 
 interface Hobbies  {
     title: string;
     description: string | null;
-
 }
 
 type Projects = { //todo interface vs type
     title: string;
     description: string;
-    techStacks: [];
+    tech_stacks: [];
     url: string;
     status: boolean;
 }
@@ -32,35 +40,91 @@ interface TechStacks {
 }
 
 interface Me {
-    name: string;
-    description: string;
-    techStacks: [];
+    username: string;
+    first_name: string;
+    last_name: string;
+    description: string; //your intro
+    age: number;
+    tech_stacks: TechStacks;
 }
 
-const setupDirectusConnection = async function() {
-    const client = createDirectus(apiURL).with(authentication()).with(rest());
-    await client.login(email, password);
-    return client;
+interface Authors {
+    id: number;
+    first_name: string;
+    last_name: string;
 }
+
+interface Books {
+    title: string;
+    description: string | null;
+    genre: [];
+    author: Authors;
+    published_date: Date; //GET YEAR ONLY
+}
+
+
 
 export async function getHobbies(): Promise<Hobbies[]> {
-    const client = await setupDirectusConnection();
     let hobbies: Hobbies[] = [];
     const result = await client.request(readItems('Hobbies'))
     hobbies = result.map((data) => ({
         title: data.title,
         description: data.description
     }))
-    // console.log('YAHOO', result);
+    console.log('hobbies: ', hobbies)
     return hobbies;
 }
 
-// TODO graphQL for these
-// export async function getProjects(): Promise<Projects[]> {
-//
-//     return
-// }
-// export async function getMe(): Promise<Me[]> {
-//
-//     return
-// }
+async function getAuthors(): Promise<Authors[]> {
+    let authors: Authors[] = [];
+    const result = await client.request(readItems('Authors'));
+    authors = result.map((data) => ({
+        id: data.id,
+        first_name: data.first_name,
+        last_name: data.last_name,
+    }))
+    console.log('authors : ', authors)
+    return authors
+}
+
+export async function getBooks(): Promise<Books[]> {
+    const authors = await getAuthors();
+    let books: Books[] = [];
+    const result = await client.request(readItems('Books'));
+    books = result.map((data) => ({
+        id: data.id,
+        title: data.title,
+        description: data.description
+    }))
+    console.log('books', books)
+    return books;
+
+}
+
+export async function getProjects(): Promise<Projects[]> {
+    let projects: Projects[] = [];
+    const result = await client.request(readItems('Books'));
+    projects = result.map((data) => ({
+        title: data.title,
+        description: data.description,
+        tech_stacks: data.tech_stacks,
+        url: data.url,
+        status: data.status
+    }))
+    console.log('projects: ',  projects)
+    return projects;
+}
+
+
+// Just for maybe summary page
+export async function getMe(): Promise<Me[]> {
+    let me: Me[] = [];
+    const res = await client.request(readItems('Me'));
+    me = res.map((data) => ({
+        name: data.name,
+        description: data.description,
+        tech_stacks: data.tech_stacks
+    }))
+    console.log('me : ', me)
+    return me;
+}
